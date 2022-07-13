d252ca3f65f0664a11f45eae6c38e59ed7c3b83d
Yevhen Yermolenko <44138260+EEM86@users.noreply.github.com>
2022-07-13 21:12:51 +0300

    Feature/brn 11 configure ci to run tests on push (#1)
    * BRN-11: add gradle project with a test
    
    * BRN-11: execute gradle test/build tasks for push and PR actions
    
    * BRN-11: fix review
    
    Co-authored-by: YYermolenko <yevhen.yermolenko-external@sema4genomics.com>

---
+++ src/test/java/com/bobocode/svydovets/DemoTest.java
@@ -0,0 +1,14 @@
+package com.bobocode.svydovets;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DemoTest {
+
+  @Test
+  void demoTest() {
+    assertEquals(2 + 2, 4);
+  }
+
+}
