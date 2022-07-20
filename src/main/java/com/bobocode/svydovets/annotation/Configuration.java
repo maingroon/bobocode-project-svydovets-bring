70dfabc5187511890b083f23c4e495803a617cdd
Oleksandr Borovyk <ob@medicosearch.ch>
2022-07-20 08:19:30 +0300

    Fixed imports checkstyle

--- src/main/java/com/bobocode/svydovets/annotation/Configuration.java
+++ src/main/java/com/bobocode/svydovets/annotation/Configuration.java
@@ -1,8 +1,8 @@
 package com.bobocode.svydovets.annotation;
 
-import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 import com.bobocode.svydovets.beans.definition.BeanDefinition;
