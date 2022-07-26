42b23831cf5711bdb465502254432c9d79f543d7
Alexey_Misroshin <oleksii.miroshin@devista.ro>
2022-07-26 11:54:16 +0300

    BRN-19 | remote extra methods, perform a bit of refactoring

---
+++ src/main/java/com/bobocode/svydovets/beans/exception/BeanInstantiationException.java
@@ -0,0 +1,7 @@
+package com.bobocode.svydovets.beans.exception;
+
+public class BeanInstantiationException extends RuntimeException {
+    public BeanInstantiationException(String message) {
+        super(message);
+    }
+}
--- src/main/java/com/bobocode/svydovets/beans/factory/DefaultListableBeanFactory.java
+++ src/main/java/com/bobocode/svydovets/beans/factory/DefaultListableBeanFactory.java
@@ -1,27 +1,35 @@
 package com.bobocode.svydovets.beans.factory;
 
 import com.bobocode.svydovets.beans.definition.BeanDefinition;
+import com.bobocode.svydovets.beans.exception.BeanInstantiationException;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class DefaultListableBeanFactory implements BeanFactory {
 
 
-    /** Create a new map of bean instances, from bean definitions.
+    /**
+     * Create a new map of bean instances, from bean definitions.
+     *
      * @param definitionMap bean definition map, key - the name of the bean, the value - bean definition object
      * @return bean map, where key - bean name, and value - new instance of the bean
      */
     @Override
-    public Map<String, Object> createBeans(Map<String, BeanDefinition> definitionMap) throws NoSuchMethodException,
-            InvocationTargetException, InstantiationException, IllegalAccessException {
-        Map<String, Object> beanMap = new HashMap<>();
-        for (Map.Entry<String, BeanDefinition> entry : definitionMap.entrySet()) {
-            Constructor<?> beanConstructor = entry.getValue().getBeanClass().getConstructor();
-            beanMap.put(entry.getKey(), beanConstructor.newInstance());
+    public Map<String, Object> createBeans(Map<String, BeanDefinition> definitionMap) {
+        return definitionMap.entrySet().stream()
+          .collect(Collectors.toMap(Map.Entry::getKey, entry -> createBean(entry.getValue())));
+    }
+
+    private static Object createBean(BeanDefinition beanDefinition) {
+        try {
+            return beanDefinition.getBeanClass().getConstructor().newInstance();
+        } catch (InvocationTargetException | InstantiationException
+          | IllegalAccessException | NoSuchMethodException e) {
+            throw new BeanInstantiationException(e.getMessage());
         }
-        return beanMap;
     }
 }
